164d4a039ceb956bf33ff03fc7a015f5cbe8e8e6
Paul Thevenot <paul.thevenot@gmail.com>
2025-11-03 21:33:43 +0100

    fix: update UserDto and site responses to reflect new API structure and optional fields

--- composeApp/src/commonMain/kotlin/net/thevenot/comwatt/model/UserDto.kt
+++ composeApp/src/commonMain/kotlin/net/thevenot/comwatt/model/UserDto.kt
@@ -9,20 +9,20 @@ data class UserDto(
     val firstName: String,
     val lastName: String,
     val email: String,
-    val newEmail: String?,
-    val pseudonym: String?,
-    val profile: ProfileDto?,
-    val address: AddressDto,
-    val phone: PhoneDto,
-    val mobilePhone: String?,
-    val currency: String,
-    val language: String,
-    val activated: Boolean,
-    val deleted: Boolean,
-    val company: String?,
-    val createDate: String,
-    val updateDate: String,
-    val agreements: AgreementsDto,
+    val newEmail: String? = null,
+    val pseudonym: String? = null,
+    val profile: ProfileDto? = null,
+    val address: AddressDto? = null,
+    val phone: PhoneDto? = null,
+    val mobilePhone: String? = null,
+    val currency: String? = null,
+    val language: String? = null,
+    val activated: Boolean? = null,
+    val deleted: Boolean? = null,
+    val company: String? = null,
+    val createDate: String? = null,
+    val updateDate: String? = null,
+    val agreements: AgreementsDto? = null,
     val uuid: String
 )
 
@@ -31,7 +31,15 @@ data class ProfileDto(
     val id: Int,
     val label: String,
     val code: String,
-    val authorities: List<String>
+    val authorities: List<String>,
+    val wattinside: Boolean? = null,
+    val supervisor: Boolean? = null,
+    val installer: Boolean? = null,
+    val admin: Boolean? = null,
+    val datascience: Boolean? = null,
+    val support: Boolean? = null,
+    val coach: Boolean? = null,
+    val globalProfile: Boolean? = null
 )
 
 @Serializable
