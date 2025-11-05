4ec01117409c769725b26e74d4291a0de4bf19a3
Paul Thevenot <paul.thevenot@gmail.com>
2025-11-05 22:55:19 +0100

    feat: add material 3 expressive

--- composeApp/src/commonMain/kotlin/net/thevenot/comwatt/ui/common/LoadingView.kt
+++ composeApp/src/commonMain/kotlin/net/thevenot/comwatt/ui/common/LoadingView.kt
@@ -1,14 +1,19 @@
 package net.thevenot.comwatt.ui.common
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ContainedLoadingIndicator
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import co.touchlab.kermit.Logger
 import comwatt.composeapp.generated.resources.Res
 import comwatt.composeapp.generated.resources.no_content_available_text
 import org.jetbrains.compose.resources.stringResource
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun LoadingView(
     isLoading: Boolean,
@@ -19,7 +24,13 @@ fun LoadingView(
     Logger.d("LoadingView") { "isLoading $isLoading, errorMessage $hasError" }
     when {
         isLoading && hasError.not() -> {
-            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                ContainedLoadingIndicator()
+            }
         }
 
         isLoading && hasError -> {
--- composeApp/src/commonMain/kotlin/net/thevenot/comwatt/ui/dashboard/pickers/WeekPicker.kt
+++ composeApp/src/commonMain/kotlin/net/thevenot/comwatt/ui/dashboard/pickers/WeekPicker.kt
@@ -1,6 +1,5 @@
 package net.thevenot.comwatt.ui.dashboard.pickers
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,11 +7,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -22,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import comwatt.composeapp.generated.resources.Res
 import comwatt.composeapp.generated.resources.week_range_selected_time_n_weeks_ago
@@ -122,24 +119,6 @@ private fun WeekRangeButton(
     item: WeekListItem.WeekInterval,
     onSelected: () -> Unit = {},
 ) {
-    val buttonColors = if (isSelected) {
-        ButtonDefaults.outlinedButtonColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-        )
-    } else {
-        ButtonDefaults.outlinedButtonColors(
-            containerColor = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.primary
-        )
-    }
-
-    val borderStroke = if (isSelected) {
-        BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
-    } else {
-        ButtonDefaults.outlinedButtonBorder()
-    }
-
     val weekTitle = when (item.index) {
         0 -> stringResource(Res.string.week_range_selected_time_past_seven_days)
         1 -> stringResource(Res.string.week_range_selected_time_one_week_ago)
@@ -149,17 +128,16 @@ private fun WeekRangeButton(
         )
     }
 
-    OutlinedButton(
-        onClick = { onSelected() },
-        colors = buttonColors,
-        border = borderStroke,
+    ToggleButton(
+        checked = isSelected, onCheckedChange = {
+            onSelected()
+        },
         modifier = Modifier
             .fillMaxWidth()
             .padding(
                 vertical = AppTheme.dimens.paddingTooSmall,
                 horizontal = AppTheme.dimens.paddingNormal
-            ),
-        shape = MaterialTheme.shapes.small
+            )
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
