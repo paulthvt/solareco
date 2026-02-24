b63c0749b76658ef4b5b32bd1993f810c998023f
Paul Thevenot <paul.thevenot@gmail.com>
2026-02-24 20:53:05 +0100

    chore: migrate to SDK 36 and enable edge-to-edge layout support

--- composeApp/src/androidMain/kotlin/net/thevenot/comwatt/utils/ScreenOrientationController.android.kt
+++ composeApp/src/androidMain/kotlin/net/thevenot/comwatt/utils/ScreenOrientationController.android.kt
@@ -2,11 +2,17 @@ package net.thevenot.comwatt.utils
 
 import android.app.Activity
 import android.content.pm.ActivityInfo
+import net.thevenot.comwatt.utils.ScreenOrientationController.lockLandscape
 import java.lang.ref.WeakReference
 
 /**
  * Android implementation of ScreenOrientationController.
  * Uses the Activity to control screen orientation.
+ *
+ * Note (Android 16 / API 36): On large-screen devices (smallest width >= 600dp),
+ * calls to [Activity.setRequestedOrientation] are ignored by the system. This means
+ * [lockLandscape] will have no effect on tablets, foldables, and desktop windows.
+ * Apps targeting SDK 36 should support adaptive layouts for all screen sizes.
  */
 actual object ScreenOrientationController {
     private var activityRef: WeakReference<Activity>? = null
